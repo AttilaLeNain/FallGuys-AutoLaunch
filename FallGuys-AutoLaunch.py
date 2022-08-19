a4cbbc8151e81c6b37d38d3cd56e37963078979d
AttilaLeNain <lerouvillois.kevin@gmail.com>
2022-08-19 22:46:05 +0200

    Update FallGuys-AutoLaunch.py

--- FallGuys-AutoLaunch.py
+++ FallGuys-AutoLaunch.py
@@ -14,40 +14,40 @@ while 1:
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Go.png', confidence=0.9):
 		keyboard.press_and_release('space')
 		print ("Démarrage du jeu!")
-		time.sleep(1)
+		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Jouer.png', confidence=0.9):
 		keyboard.press_and_release('space')
 		print ("Lancement d'une partie")
-		time.sleep(1)
+		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Retour.png', confidence=0.9):
 		keyboard.press_and_release('esc')
-		time.sleep(1) 
+		time.sleep(0.5) 
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Ok bleu.png', confidence=0.9):
 		keyboard.press_and_release('space')
 		print ("C'est fini!")
-		time.sleep(1)
+		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Ok recompense.png', confidence=0.9):
 		keyboard.press_and_release('space')
 		print ("Récompense récupéré!")
-		time.sleep(1)     
+		time.sleep(0.5)     
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Confirmer.png', confidence=0.9):
 		keyboard.press_and_release('space')
 		print ("Retour à l'acceuil")
-		time.sleep(1)  
+		time.sleep(0.5)  
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Quitter.png', confidence=0.9):
 		keyboard.press_and_release('esc')
-		time.sleep(1)  
+		time.sleep(0.5)  
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Rester quitter.png', confidence=0.9):
 		keyboard.press_and_release('esc')
-		time.sleep(1)   
+		time.sleep(0.5)   
 
 	#if pyautogui.locateOnScreen(f'{main_path}\image\-Ok rouge.png', confidence=0.9):
 		keyboard.press_and_release('esc')    
-		time.sleep(1)
+		time.sleep(0.5)
