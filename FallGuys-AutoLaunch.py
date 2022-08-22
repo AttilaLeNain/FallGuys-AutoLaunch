25729584397b81466b4b26484b0bce77ff97a79a
AttilaLeNain <lerouvillois.kevin@gmail.com>
2022-08-22 15:23:40 +0200

    Update

--- FallGuys-AutoLaunch.py
+++ FallGuys-AutoLaunch.py
@@ -8,22 +8,24 @@ import win32api, win32con
 main_path = Path(__file__).parent
 
 while 1:
+	t = time.localtime()
+	current_time = time.strftime("%H:%M:%S", t)
 	if keyboard.is_pressed("P"):
 		break
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Go.png', confidence=0.9):
 		keyboard.press_and_release('space')
-		print ("Démarrage du jeu!")
+		print (current_time,"Démarrage du jeu!")
 		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Deco.png', confidence=0.9):
 		keyboard.press_and_release('space')
-		print ("Reconnexion!")
+		print (current_time,"Reconnexion!")
 		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Jouer.png', confidence=0.9):
 		keyboard.press_and_release('space')
-		print ("Lancement d'une partie")
+		print (current_time,"Lancement d'une partie")
 		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Retour.png', confidence=0.9):
@@ -32,17 +34,17 @@ while 1:
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Ok bleu.png', confidence=0.9):
 		keyboard.press_and_release('space')
-		print ("C'est fini!")
+		print (current_time,"C'est fini!")
 		time.sleep(0.5)
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Ok recompense.png', confidence=0.9):
 		keyboard.press_and_release('space')
-		print ("Récompense récupéré!")
+		print (current_time,"Récompense récupéré!")
 		time.sleep(0.5)     
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Confirmer.png', confidence=0.9):
 		keyboard.press_and_release('space')
-		print ("Retour à l'acceuil")
+		print (current_time,"Retour à l'acceuil")
 		time.sleep(0.5)  
 
 	if pyautogui.locateOnScreen(f'{main_path}\image\-Quitter.png', confidence=0.9):
